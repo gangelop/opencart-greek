741cd18b0929859d59b134b31adaba357f552d7b
George Angelopoulos <george@usermod.net>
2013-10-22 12:10:57 +0300

    adds cart module

---
+++ greek/module/cart.php
@@ -0,0 +1,12 @@
+<?php
+// Heading 
+$_['heading_title'] = 'Καλάθι Αγορών';
+
+// Text
+$_['text_items']    = '%s αντικείμενα- %s';
+$_['text_empty']    = 'Το καλάθι σας είναι άδειο!';
+$_['text_cart']     = 'Προβολή Καλαθιού';
+$_['text_checkout'] = 'Αγορά';
+
+$_['text_payment_profile'] = 'Payment Profile';
+?>
