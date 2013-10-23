03bb7b9d9e0a0b3d7d3f34649c22dfc1dbf7dcca
George Angelopoulos <george@usermod.net>
2013-10-23 14:15:30 +0300

    fixes shipping/royal_mail.php

--- greek/shipping/royal_mail.php
+++ greek/shipping/royal_mail.php
@@ -3,11 +3,13 @@
 $_['text_title']                = 'Royal Mail';
 $_['text_weight']               = 'Βάρος:';
 $_['text_insurance']            = 'Ασφαλισμένο μέχρι:';
-$_['text_eta']                  = 'Εκτιμώμενος χρόνος:';
 $_['text_1st_class_standard']   = 'Πρώτης Κατηγορίας Βασική Αποστολή';
 $_['text_1st_class_recorded']   = 'Πρώτης Κατηγορίας Βασική Καταγεγραμμένη Αποστολή';
 $_['text_2nd_class_standard']   = 'Δεύτερης Κατηγορίας Βασική Αποστολή';
 $_['text_2nd_class_recorded']   = 'Δεύτερης Κατηγορίας Καταγεγραμμένη Αποστολή';
+$_['text_special_delivery_500']  = 'Ειδική Αποστολή Επόμενης Μέρας (&pound;500)';
+$_['text_special_delivery_1000'] = 'Ειδική Αποστολή Επόμενης Μέρας (&pound;1000)';
+$_['text_special_delivery_2500'] = 'Ειδική Αποστολή Επόμενης Μέρας (&pound;2500)';
 $_['text_standard_parcels']     = 'Βασικά Πακέτα';
 $_['text_airmail']              = 'Αεροπορικώς';
 $_['text_international_signed'] = 'Συστημένη Διεθνής';
