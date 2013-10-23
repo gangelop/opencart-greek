9acf2c66ffdd3face9b73ee8f3ee63848a4d79b7
George Angelopoulos <george@usermod.net>
2013-10-23 13:51:28 +0300

    adds payment/amazon_checkout.php

---
+++ greek/payment/amazon_checkout.php
@@ -0,0 +1,28 @@
+<?php
+//Headings
+$_['heading_title'] = 'Amazon Payments';
+$_['heading_address'] = 'Παρακαλώ επιλέξτε διεύθυνση παράδοσης';
+$_['heading_payment'] = 'Παρακαλώ επιλέξτε τρόπο πληρωμής';
+$_['heading_confirm'] = 'Περίληψη Παραγγελίας';
+
+//Text
+$_['text_back'] = 'Πίσω';
+$_['text_cart'] = 'Καλάθι';
+$_['text_confirm'] = 'Επιβεβαίωση';
+$_['text_continue'] = 'Συνέχεια';
+$_['text_cba'] = 'Amazon Payments';
+$_['text_enter_coupon'] = 'Εισάγετε το κουπόνι σας εδώ. Αν δεν έχετε, αφήστε το πεδίο άδειο.';
+$_['text_coupon'] = 'Κουπόνι';
+$_['text_tax_other'] = 'Φόροι / Λοιπές Χρεώσεις';
+$_['text_payment_failed'] = 'Η πληρωμή σας απέτυχε. Παρακαλώ επικοινωνήστε με τον διαχειριστή του καταστήματος για εξυπηρέτηση με διαφορετική μέθοδο πληρωμής.';;
+$_['text_payment_success'] = 'Your order was successfully placed. Order\'s details are below';
+$_['text_payment_success'] = 'Η παραγγελία σας ολοκληρώθηκε με επιτυχία. Ακολουθούν λεπτομέριες';
+
+//Errors
+$_['error_payment_method'] = 'Παρακαλώ επιλέξτε τρόπο πληρωμής';;
+$_['error_shipping'] = 'Παρακαλώ επιλέξτε τρόπο αποστολής';;
+$_['error_shipping_address'] = 'Παρακαλώ επιλέξτε διεύθυνση παράδοσης';
+$_['error_shipping_methods'] = 'Παρουσιάστηκε σφάλμα κατα την ανάκτηση της διεύθυνσής σας από το Amazon. Παρακαλώ επικοινωνήστε με τον διαχειριστή του καταστήματος για βοήθεια.';
+$_['error_no_shipping_methods'] = 'There are no shipping options to the selected address. Please select a different shipping address';
+$_['error_no_shipping_methods'] = 'Δεν υπάρχουν επιλογές αποστολής για την διεύθυνση που επιλέξατε. Παρακαλώ επιλέξτε μια άλλη διεύθυνση παράδοσης';
+?>
