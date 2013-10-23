074b2ac69260ee218409ae1c7cb4a648e2ef0e61
George Angelopoulos <george@usermod.net>
2013-10-23 12:41:38 +0300

    adds payment/pp_express.php

--- greek/payment/pp_express.php
+++ greek/payment/pp_express.php
@@ -1,4 +1,22 @@
 <?php
 // Text
-$_['text_title'] = 'PayPal Express (συμπεριλαμβανομένων Πιστωτικών και Χρεωστικών Καρτών)';
+$_['text_title']                    = 'PayPal Express Checkout';
+$_['button_continue']               = 'Συνέχεια';
+$_['text_cart']                     = 'Καλάθι';
+$_['text_shipping_updated']         = 'Υπηρεσία αποστολής ενημερώθηκε';
+$_['text_trial']                    = '%s κάθε %s %s για %s πληρωμές τότε';
+$_['text_recurring']                = '%s κάθε %s %s';
+$_['text_length']                   = ' για %s πληρωμές';
+
+// Standard checkout error page
+$_['error_heading_title']           = 'Υπήρξε ένα σφάλμα';
+$_['error_too_many_failures']       = 'Η πληρωμή σας έχει αποτύχει πολλές φορές';
+
+// Express confirm page
+$_['express_text_title']            = 'Επιβεβαίωση Παραγγελίας';
+$_['express_button_coupon']         = 'Προσθήκη';
+$_['express_button_confirm']        = 'Επιβεβαίωση';
+$_['express_button_login']          = 'Συνεχίστε στην PayPal';
+$_['express_button_shipping']       = 'Ανανέωση πληροφοριών αποστολής';
+$_['express_entry_coupon']          = 'Εισάγετε το κουπόνι σας εδώ:';
 ?>
