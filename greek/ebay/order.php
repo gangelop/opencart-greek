bb733c7257f7c08ef8e88185d327a9989ec2bba7
George Angelopoulos <george@usermod.net>
2013-10-23 15:18:18 +0300

    adds ebay

---
+++ greek/ebay/order.php
@@ -0,0 +1,6 @@
+<?php
+$_['lang_shipping']     = 'Μεταφορικά';
+$_['lang_discount']     = 'Έκπτωση';
+$_['lang_tax']          = 'Φόρος';
+$_['lang_subtotal']     = 'Υποσύνολο';
+$_['lang_total']        = 'Συνολο';
---
+++ greek/ebay/stock_report.php
@@ -0,0 +1,3 @@
+<?php
+$_['title']         = 'OpenBay Pro stock link report';
+$_['help']          = 'Κάντε κλικ εδώ για βοήθεια';
