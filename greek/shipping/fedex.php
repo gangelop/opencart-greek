3456a653305993678a552c3964d08e93b18ecfac
George Angelopoulos <george@usermod.net>
2013-10-23 14:30:12 +0300

    adds shipping/fedex.php

---
+++ greek/shipping/fedex.php
@@ -0,0 +1,27 @@
+<?php
+// Text
+$_['text_title']                               = 'Fedex';
+$_['text_weight']                              = 'Βάρος:';
+$_['text_eta']                                 = 'Αναμενόμενος Χρόνος Άφιξης:';
+$_['text_europe_first_international_priority'] = 'Ευρώπη Πρώτης Προτεραιότητας';
+$_['text_fedex_1_day_freight']                 = 'Fedex 1 Ημέρα Freight';
+$_['text_fedex_2_day']                         = 'Fedex 2 Ημέρα ';
+$_['text_fedex_2_day_am']                      = 'Fedex 2 Ημέρες AM';
+$_['text_fedex_2_day_freight']                 = 'Fedex 2 Ημέρες Freight';
+$_['text_fedex_3_day_freight']                 = 'Fedex 3 Ημέρες Freight';
+$_['text_fedex_express_saver']                 = 'Fedex Express Οικονομικό';
+$_['text_fedex_first_freight']                 = 'Fedex Πρώτο Fright';
+$_['text_fedex_freight_economy']               = 'Fedex Fright Οικονομικό';
+$_['text_fedex_freight_priority']              = 'Fedex Fright Προτεραιότητας';
+$_['text_fedex_ground']                        = 'Fedex Ground';
+$_['text_first_overnight']                     = 'First Overnight';
+$_['text_ground_home_delivery']                = 'Ground Home Delivery';
+$_['text_international_economy']               = 'Διεθνές Οικονομικό';
+$_['text_international_economy_freight']       = 'Διεθνές Οικονομικό Freight';
+$_['text_international_first']                 = 'Διεθνές Πρώτο';
+$_['text_international_priority']              = 'Διεθνές Προτεραιότητας';
+$_['text_international_priority_freight']      = 'Διεθνές Προτεραιότητας Freight';
+$_['text_priority_overnight']                  = 'Προτεραιότητας Overnight';
+$_['text_smart_post']                          = 'Smart Post';
+$_['text_standard_overnight']                  = 'Standard Overnight';
+?>
