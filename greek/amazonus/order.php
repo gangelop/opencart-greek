7edf128b490064a2a08c6c82970f83f29b445562
George Angelopoulos <george@usermod.net>
2013-10-23 15:16:49 +0300

    adds amazonus/order.php

---
+++ greek/amazonus/order.php
@@ -0,0 +1,10 @@
+<?php
+
+$_['paid_on_amazonus_text'] = 'Πληρωμή στο Amazon';
+$_['shipping_text'] = 'Μεταφορικά';
+$_['shipping_tax_text'] = 'Φόρος Μεταφορικών';
+$_['gift_wrap_text'] = 'Συσκευασία Δώρου';
+$_['gift_wrap_tax_text'] = 'Φόρος Συσκευασίας Δώρου';
+$_['sub_total_text'] = 'Υποσύνολο';
+$_['tax_text'] = 'Φόρος';
+$_['total_text'] = 'Σύνολο'; 
