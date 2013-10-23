cadb615ee86765ad62bdf8598a62bf904f41bc96
George Angelopoulos <george@usermod.net>
2013-10-23 12:10:36 +0300

    fixes module/account.php

--- greek/module/account.php
+++ greek/module/account.php
@@ -10,10 +10,12 @@ $_['text_forgotten']   = 'Απώλεια Κωδικού';
 $_['text_account']     = 'Ο Λογαριασμός μου';
 $_['text_edit']        = 'Επεξεργασία Λογαριασμού';
 $_['text_password']    = 'Κωδικός';
+$_['text_address']     = 'Βιβλία Διευθύνσεων';
 $_['text_wishlist']    = 'Λίστα Επιθυμιών';
 $_['text_order']       = 'Ιστορικό Παραγγελιών';
 $_['text_download']    = 'Λήψεις';
 $_['text_return']      = 'Επιστροφές';
 $_['text_transaction'] = 'Συναλλαγές';
 $_['text_newsletter']  = 'Ενημερωτικό';
+$_['text_recurring']   = 'Επαναλαμβανόμενες Πληρωμές';
 ?>
