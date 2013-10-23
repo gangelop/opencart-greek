523f653b2cf7afd46878464232d1e67cedded413
George Angelopoulos <george@usermod.net>
2013-10-23 14:57:12 +0300

    adds account/recurring.php

---
+++ greek/account/recurring.php
@@ -0,0 +1,54 @@
+<?php
+$_['heading_title'] = 'Επαναλαμβανόμενες Πληρωμές';
+$_['button_continue'] = 'Συνέχεια';
+$_['button_view'] = 'Προβολή';
+$_['button_cancel_profile'] = 'Ακύρωση Πληρωμών';
+$_['text_empty'] = 'Δεν βρέθηκαν επαναλαμβανόμενες πληρωμές';;
+$_['text_product'] = 'Προϊόν: ';
+$_['text_order'] = 'Παραγγελία: ';
+$_['text_quantity'] = 'Ποσότητα: ';
+$_['text_account'] = 'Λογαριασμός';
+$_['text_action'] = 'Ενέργεια';
+$_['text_recurring'] = 'Επαναλαμβανόμενη Πληρωμή';
+$_['text_transactions'] = 'Συναλλαγές';
+$_['button_return'] = 'Επιστροφή';
+$_['text_confirm_cancel'] = 'Είστε σίγουρος/η ότι θέλετε να ακυρώσετε αυτό το προφίλ;';
+$_['text_empty_transactions'] = 'Δεν υπάρχουν συναλλαγές για αυτό το προφιλ';
+
+$_['column_created'] = 'Δημιουργήθηκε';
+$_['column_type'] = 'Είδος';
+$_['column_amount'] = 'Ποσό';
+$_['column_status'] = 'Κατάσταση';
+$_['column_product'] = 'Προϊόν';
+$_['column_action'] = 'Ενέργεια';
+$_['column_profile_id'] = 'Κωδικός Προφίλ';
+
+$_['text_recurring_detail'] = 'Λεπτομέρειες Επαναλλαμβανόμενης Πληρωμής';
+$_['text_recurring_id'] = 'Κωδικός Προφίλ: ';
+$_['text_payment_method'] = 'Τρόπος Πληρωμής: ';
+$_['text_date_added'] = 'Δημιουργήθηκε: ';
+$_['text_recurring_description'] = 'Περιγραφή: ';
+$_['text_status'] = 'Κατάταση: ';
+$_['text_ref'] = 'Reference: ';
+
+$_['text_status_active'] = 'Ενεργό';
+$_['text_status_inactive'] = 'Ανενεργό';
+$_['text_status_cancelled'] = 'Ακυρωμένο';
+$_['text_status_suspended'] = 'Ανεσταλμένο';
+$_['text_status_expired'] = 'Ληγμένο';
+$_['text_status_pending'] = 'Σε Εκκρεμότητα';
+
+$_['text_transaction_created'] = 'Δημιουργήθηκε';
+$_['text_transaction_payment'] = 'Πληρωμή';
+$_['text_transaction_outstanding_payment'] = 'Πληρωμή σε εκκρεμότητα';
+$_['text_transaction_skipped'] = 'Πληρωμή Παραλείφθηκε';
+$_['text_transaction_failed'] = 'Πληρωμή Απέτυχε';
+$_['text_transaction_cancelled'] = 'Aκυρώμένο';
+$_['text_transaction_suspended'] = 'Ανεσταλμένο';
+$_['text_transaction_suspended_failed'] = 'Ανεσταλμένο λόγω αποτυχίας πληρωμής';
+$_['text_transaction_outstanding_failed'] = 'Αποτυχία Πληρωμής σε εκκρεμότητα';
+$_['text_transaction_expired'] = 'Ληγμένο';
+
+$_['error_not_cancelled'] = 'Σφάλμα: %s';
+$_['error_not_found'] = 'Δεν ήταν δυνατό να ακυρωθεί το προφίλ';
+$_['success_cancelled'] = 'Η επαναλαμβανόμενη πληρωμή έχει ακυρωθεί';
