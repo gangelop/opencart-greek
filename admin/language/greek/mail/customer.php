243d55e5328a86aa28d60ca3f5c664df2a082405
George Angelopoulos <george@usermod.net>
2013-11-06 15:49:24 +0200

    in admin, translates mail/customer.php

--- admin/language/greek/mail/customer.php
+++ admin/language/greek/mail/customer.php
@@ -1,14 +1,14 @@
 <?php
 // Text
-$_['text_approve_subject']      = '%s - Your Account has been activated!';
-$_['text_approve_welcome']      = 'Welcome and thank you for registering at %s!';
-$_['text_approve_login']        = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_approve_services']     = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_approve_thanks']       = 'Thanks,';
-$_['text_transaction_subject']  = '%s - Account Credit';
-$_['text_transaction_received'] = 'You have received %s credit!';
-$_['text_transaction_total']    = 'Your total amount of credit is now %s.' . "\n\n" . 'Your account credit will be automatically deducted from your next purchase.';
-$_['text_reward_subject']       = '%s - Reward Points';
-$_['text_reward_received']      = 'You have received %s Reward Points!';
-$_['text_reward_total']         = 'Your total number of reward points is now %s.';
+$_['text_approve_subject']      = '%s - Ο λογαριασμός σας έχει ενεργοποιηθεί!';
+$_['text_approve_welcome']      = 'Καλώς ήρθατε και ευχαριστούμε για την εγγραφή σας στο  %s!';
+$_['text_approve_login']        = 'Ο λογαριασμός σας έχει ενεργοποιηθεί και μπορείτε να εισέλθετε χρησιμοποιώντας τη διεύθυνση e-mail και τον κωδικό σας ακολουθώντας τον παρακάτω σύνδεσμο:';
+$_['text_approve_services']     = 'Μόλις συνδεθείτε, θα έχετε πρόσβαση σε υπηρεσίες όπως προβολή προηγούμενων παραγγελιών σας, εκτύπωση αποδείξεων και αλλαγή των πληροφοριών του λογαριασμού σας.';
+$_['text_approve_thanks']       = 'Ευχαριστούμε,';
+$_['text_transaction_subject']  = '%s - Πίστωση Λογαριασμού';
+$_['text_transaction_received'] = 'Έχετε λάβει %s πίστωση!';
+$_['text_transaction_total']    = 'Το συνολικό ποσό πίστωσής σας τώρα είναι %s.' . "\n\n" . 'Η πίστωση στον λογαριασμό σας θα αφαιρεθεί αυτόματα από την επόμενη αγορά σας.';
+$_['text_reward_subject']       = '%s - Πόντοι Ανταμοιβής';
+$_['text_reward_received']      = 'Έχετε λάβει %s πόντους ανταμοιβής!';
+$_['text_reward_total']         = 'Το σύνολο πόντων ανταμοιβής σας τώρα είναι %s.';
 ?>
