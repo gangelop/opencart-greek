e116b210cfa55086463d678550bd8d5d64dc460a
George Angelopoulos <george@usermod.net>
2013-11-13 19:39:18 +0200

    cleanup account module
    Make strings in account module less verbose.
    Less verbose is better.

--- greek/module/account.php
+++ greek/module/account.php
@@ -9,13 +9,13 @@ $_['text_logout']      = 'Αποσύνδεση';
 $_['text_forgotten']   = 'Ξέχασα τον κωδικό!';
 $_['text_account']     = 'Ο Λογαριασμός μου';
 $_['text_edit']        = 'Επεξεργασία Λογαριασμού';
-$_['text_password']    = 'Κωδικός';
-$_['text_address']     = 'Βιβλία Διευθύνσεων';
+$_['text_password']    = 'Αλλαγή Κωδικού';
+$_['text_address']     = 'Διευθύνσεις';
 $_['text_wishlist']    = 'Λίστα Επιθυμιών';
-$_['text_order']       = 'Ιστορικό Παραγγελιών';
+$_['text_order']       = 'Ιστορικό';
 $_['text_download']    = 'Λήψεις';
 $_['text_return']      = 'Επιστροφές';
 $_['text_transaction'] = 'Συναλλαγές';
-$_['text_newsletter']  = 'Ενημερωτικό';
+$_['text_newsletter']  = 'Ενημερώσεις';
 $_['text_recurring']   = 'Επαναλαμβανόμενες Πληρωμές';
 ?>
