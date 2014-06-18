45be0ef682aed47cdbcd34a9a9128e3d62c8ff2a
Vagelis Paschaloudis <dino_sauro00@yahoo.com>
2014-06-18 20:01:00 +0200

    Clearer translation

--- catalog/language/greek/common/header.php
+++ catalog/language/greek/common/header.php
@@ -4,7 +4,7 @@ $_['text_home']     = 'Αρχική';
 $_['text_wishlist'] = 'Λίστα Επιθυμιών (%s)';
 $_['text_shopping_cart']     = 'Καλάθι Αγορών';
 $_['text_search']   = 'Αναζήτηση';
-$_['text_welcome']  = '<a href="%s">Σύνδεση</a> / <a href="%s">Εγγραφή</a>';
+$_['text_welcome']  = '<a href="%s">Είσοδος</a> / <a href="%s">Εγγραφή</a>';
 $_['text_logged']   = 'Έχετε εισέλθει ως <a href="%s">%s</a> <b>(</b> <a href="%s">Αποσύνδεση</a> <b>)</b>';
 $_['text_account']  = 'Ο Λογαριασμός μου';
 $_['text_checkout'] = 'Αγορά';
